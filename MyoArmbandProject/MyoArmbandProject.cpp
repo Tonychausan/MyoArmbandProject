4d653a3d749abd5f78e0b8e14a2a0a4933e11ca8
Tony Chau <tony0chau@gmail.com>
2017-01-13 20:47:12 +0100

    hello

--- MyoArmbandProject/MyoArmbandProject.cpp
+++ MyoArmbandProject/MyoArmbandProject.cpp
@@ -19,6 +19,8 @@
 
 #include "Utility.h"
 #include "DataCollector.h"
+#include "MenuActions.h"
+#include "Constants.h"
 
 // The only file that needs to be included to use the Myo C++ SDK is myo.hpp.
 #include <myo/myo.hpp>
@@ -27,6 +29,52 @@
 
 volatile bool isRecording = false;
 volatile bool dummy = false;
+
+bool isMyoFound = false;
+
+DataCollector collector;
+
+void findMyo(myo::Hub &hub){
+	std::cout << "Attempting to find a Myo..." << std::endl;
+
+	// Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
+	// immediately.
+	// waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
+	// if that fails, the function will return a null pointer.
+	myo::Myo* myo = hub.waitForMyo(10000);
+
+	// If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
+	if (!myo) {
+		throw std::runtime_error("Unable to find a Myo!");
+		std::cin.ignore();
+	}
+
+	// We've found a Myo.
+	std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
+	isMyoFound = true;
+
+	// Next we enable EMG streaming on the found Myo.
+	myo->setStreamEmg(myo::Myo::streamEmgEnabled);
+
+	// Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
+	// Hub::run() to send events to all registered device listeners.
+	hub.addListener(&collector);
+}
+
+void printMenu(){
+	clearScreen();
+	std::cout << "Choose an action" << std::endl;
+	std::cout << "1) Try gestures" << std::endl;
+	std::cout << "2) Measurment display" << std::endl;
+	std::cout << "3) Compress files" << std::endl;
+	std::cout << "4) Pre-data gesture comparison" << std::endl;
+}
+
+void menu(int& action){
+	action = 0;
+	printMenu();
+	std::cin >> action;
+}
 
 void keyboardInterruptDetector()
 {
@@ -47,20 +95,6 @@ void keyboardInterruptDetector()
 	
 }
 
-void testFile(std::string filename, Gesture gesture){
-	std::cout << "##############################"<< std::endl << "Test file: " << filename << std::endl;
-
-	Gesture prediction = gestureComparisonsJsonFile(getCompressedFilename(filename));
-	std::cout << std::endl << "Gesture: " << gestureToString(gesture) << std::endl;
-	std::cout << "Prediction: " << gestureToString(prediction) << std::endl << std::endl;
-
-	numberOfTestsForGesture[gesture] += 1;
-	if (prediction == gesture)
-	{
-		correctTests[gesture] += 1;
-	}
-}
-
 void initTest(){
 	for (int i = 0; i < NUMBER_OF_GESTURES; i++)
 	{
@@ -73,114 +107,78 @@ int main(int argc, char** argv)
 {
 	// We catch any exceptions that might occur below -- see the catch statement for more details.
 	// Action menu
-	int action = 0;
+	int action;
 
+START_MENU:
+	menu(action);
 
-	try {
-		// First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
-		// publishing your application. The Hub provides access to one or more Myos.
-		myo::Hub hub("com.example.hello-myo");
-
-		std::cout << "Attempting to find a Myo..." << std::endl;
-
-		// Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
-		// immediately.
-		// waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
-		// if that fails, the function will return a null pointer.
-		myo::Myo* myo = hub.waitForMyo(10000);
-
-		// If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
-		if (!myo) {
-			throw std::runtime_error("Unable to find a Myo!");
-			std::cin.ignore();
-		}
-
-		// We've found a Myo.
-		std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
-
-		// Next we enable EMG streaming on the found Myo.
-		myo->setStreamEmg(myo::Myo::streamEmgEnabled);
-
-		// Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
-		DataCollector collector;
-
-		// Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
-		// Hub::run() to send events to all registered device listeners.
-		hub.addListener(&collector);
-			
-		// Finally we enter our main loop.
-		isProgramRunning = false;
-
-		while (true){
-			clearScreen();
-			std::cout << "Choose an action" << std::endl;
-			std::cout << "1) Try gestures" << std::endl;
-			std::cout << "2) Measurment display" << std::endl;
-			std::cout << "3) Compress files" << std::endl;
-			std::cout << "4) Pre-data gesture comparison" << std::endl;
-			std::cin >> action;
-
-			// Compress files
-			if (action == 3){
-				compressAllJsonFiles();
-				std::cin.ignore();
-				clearScreen();
+	if (action == 4)
+		preDataTest();
+	else if (action == 3)
+		compressFiles();
+	else{
+		try {
+			// First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
+			// publishing your application. The Hub provides access to one or more Myos.
+			myo::Hub hub("com.example.hello-myo");
+			if (!isMyoFound){
+				findMyo(hub);
 			}
-			// Print Measurment
-			else if (action == 2){
-				isProgramRunning = true;
-				while (isProgramRunning) {
-					// In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
-					// In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
-					hub.run(1000 / 5);
 
-					clearScreen();
+			// Finally we enter our main loop.
+			isProgramRunning = false;
 
-					collector.printStatus();
-					collector.printEMG();
-					collector.printAccelerometer();
-					collector.printGyro();
-					collector.printOrientation();
+			while (true){
+				// Compress files
+				if (action == 3){
+					compressFiles();
 				}
-			}
-			// Test gestures with pre-sampled file tests
-			else if (action == 4){
-				initTest();
-				for (int i = 0; i < preSampledRecordFileListSize; i++)
-				{
-					testFile(preSampledRecordFileList[i], preSampledRecordPredictionList[i]);
+				// Test gestures with pre-sampled file tests
+				else if (action == 4){
+					preDataTest();
 				}
+				// Print Measurment
+				else if (action == 2){
+					isProgramRunning = true;
+					while (isProgramRunning) {
+						// In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
+						// In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
+						hub.run(1000 / 5);
 
-				for (int i = 0; i < NUMBER_OF_GESTURES; i++)
-				{
-					std::cout << gestureToString(static_cast<Gesture>(i)) << ": " << correctTests[i] << " of " << numberOfTestsForGesture[i] << std::endl;
-				}
+						clearScreen();
 
-				system("PAUSE");
-			}
-			// Try some gesture recording
-			else {
-				isProgramRunning = true;
-				std::cout << RECORD_PRESTART_MESSEGE << std::endl;
-				std::thread keyboardInterrupt(keyboardInterruptDetector);
-
-
-				while (isProgramRunning){
-					if (isRecording){
-						collector.gestureRecordOn();
-						isRecording = false;
+						collector.printStatus();
+						collector.printEMG();
+						collector.printAccelerometer();
+						collector.printGyro();
+						collector.printOrientation();
 					}
-					hub.run(1000 / 5);
 				}
-				keyboardInterrupt.join();
+				// Try some gesture recording
+				else {
+					isProgramRunning = true;
+					std::cout << RECORD_PRESTART_MESSEGE << std::endl;
+					std::thread keyboardInterrupt(keyboardInterruptDetector);
+
+
+					while (isProgramRunning){
+						if (isRecording){
+							collector.gestureRecordOn();
+							isRecording = false;
+						}
+						hub.run(1000 / 5);
+					}
+					keyboardInterrupt.join();
+				}
+
+				menu(action);
 			}
 		}
-	}
-	catch (const std::exception& e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-		std::cerr << "Press enter to continue.";
-		std::cin.ignore();
-		return 1;
+		catch (const std::exception& e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+			system("PAUSE");
+			goto START_MENU;
+		}
 	}
 
 	system("PAUSE");
